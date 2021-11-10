9d0eb604d430ea9bc08b5e72ceaa3fe47377c32f
nikhilspatil103 <nikhilspatil103@gmail.com>
2021-11-10 18:53:25 +0530

    W4 D2 Assignment

--- src/index.js
+++ src/index.js
@@ -13,3 +13,8 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function() {
 	console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+// localhost:3000/movies(localhost: local server,3000:port) (port means use to run on OS System .tecnology port)
+// ip adddress
+//127.44(something):3000/test-me
+// port number internal communication of api
--- src/routes/route.js
+++ src/routes/route.js
@@ -6,4 +6,45 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-module.exports = router;
+//!-------------------------2nd API---------------------------------------------//
+
+router.get('/movies', function (req, res) {
+  
+    res.send(["3idiots","welcome","herapheri"])
+});
+
+//!--------------------------3rd API--------------------------------------------//
+
+router.get('/movies/:moviesId', function(req,res){
+    
+     function a(arr){      
+        let value=req.params.moviesId
+       if(value<arr.length){
+              res.send(arr[value]) 
+        }else{
+              res.send("Please enter valid index")
+         }
+     }
+    res.send(a(["3idiots","welcome","herapheri"]))
+})
+//!---------------------------4th API-------------------------------------------//
+
+router.get('/flims',function(req,res){
+    let movie=[{"id":1,"name":"3idiots"}, {"id":2,"name": "welcome"}, {"id":3,"name":"heraferi"},{"id":4,"name":"run"}]
+    res.send(movie)
+})
+
+//!---------------------------5th API-------------------------------------------//
+
+router.get('/flims/:flimId', function(req,res){
+    let movie=[{"id":1,"name":"3idiots"}, {"id":2,"name": "welcome"}, {"id":3,"name":"heraferi"},{"id":4,"name":"run"}]
+    let value=req.params.flimId
+
+    if(value<movie.length){
+        res.send(movie[value])
+    }else {
+       res.send("No movie exists with this id")
+    }    
+
+})
+module.exports = router; 
